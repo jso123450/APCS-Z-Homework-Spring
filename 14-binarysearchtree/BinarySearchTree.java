7a74bb33181431aaf36b7eb6c1a0660fe8d804f9
Johnny So <jso123450@gmail.com>
2015-04-24 08:34:53 -0400

    HW#14: BinarySearchTree w/ Recursion javac BinarySearchTree.javajavac BinarySearchTree.java :)

--- 14-binarysearchtree/BinarySearchTree.java
+++ 14-binarysearchtree/BinarySearchTree.java
@@ -6,6 +6,8 @@ public class BinarySearchTree {
 	this.root = root;
     }
 
+    /* -------------------------- LOOPING -------------------------- */
+
     // within itself
     public Node search(int i){
         Node output = search(root,i);
@@ -18,9 +20,9 @@ public class BinarySearchTree {
 	while (n != null){
 	    int c = n.getData().compareTo(i);
 	    if (c > 0)
-		n = n.getRight();
-	    else if (c < 0)
 		n = n.getLeft();
+	    else if (c < 0)
+		n = n.getRight();
 	    else
 		return n;
 	}
@@ -42,16 +44,69 @@ public class BinarySearchTree {
 	Node piggybacker = null;
 	while (iter != null){
 	    piggybacker = iter;
-	    int c = n.getData().compareTo(i);
+	    int c = t.getData().compareTo(i);
 	    if (c < 0)
-		iter = iter.getLeft();
-	    else if (c > 0)
 		iter = iter.getRight();
+	    else if (c > 0)
+		iter = iter.getLeft();
 	}
 	if (i > piggybacker.getData())
 	    piggybacker.setRight(insertion);
 	else if (i < piggybacker.getData())
 	    piggybacker.setLeft(insertion);
     }
+
+    /* -------------------------- RECURSIVE -------------------------- */
+
+    // within its own tree
+    public Node recSearch(int i){
+	Node r = root;
+	r = recSearch(r,i);
+	return r;
+    }
+
+    public Node recSearch(Node t, int i){
+	if (t == null)
+	    return null;
+	else {
+	    int c = t.getData().compareTo(i);
+	    if (c == 0)
+		return t;
+	    else if (c < 0)
+		t = t.getRight();
+	    else if (c > 0)
+		t = t.getLeft();
+	    this.recSearch(t,i);
+	}
+	return null;
+    }
+
+    public void recInsert(Node t, int i){
+	Node insertion = new Node(i);
+	if (t == null){
+	    t = insertion;
+	}
+	else {
+	    int c = t.getData().compareTo(i);
+	    if (c > 0){
+		if (t.getLeft() == null){
+		    t.setLeft(insertion);
+		    return;
+		}
+		else
+		    t = t.getLeft();
+	    }
+	    else if (c < 0){
+		if (t.getRight() == null){
+		    t.setRight(insertion);
+		    return;
+		}
+		else
+		    t = t.getRight();
+	    }
+	    this.recInsert(t,i);
+	}
+    }
+
 
 }
