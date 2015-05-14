0f123eb10f2f95e7324e5870471e80e849d9128f
Johnny So <jso123450@gmail.com>
2015-05-14 08:37:18 -0400

    Heap

---
+++ 16-heap/Heap.java
@@ -0,0 +1,28 @@
+public class Heap {
+
+    /* ------------------------ SETUP ------------------------ */
+
+    private Node root;
+    private ArrayList<Node> a;
+
+    public Heap(Node root){
+	setRoot(root);
+	a = new ArrayList<Integer>();
+	a.add(root);
+    }
+
+    public void setRoot(Node root){
+	this.root = root;;
+    }
+
+    public Integer getRoot(){
+	return root.getData();
+    }
+
+    /* ------------------------ HEAP STUFF ------------------------ */
+
+    public void pushDown(){
+
+    }
+
+}
